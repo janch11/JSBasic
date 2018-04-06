db8293e13cbed453ea0e9214723a029ae354af1c
Jan Choma <jandominikchoma@gmail.com>
2018-04-06 10:49:37 +0200

    falsy or 1

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-var ages = 23 + 24 + 25 + 34
-var count =  ages / 4
 
-if (count % 2 == 0) {
-    alert("Is Even")
-}
-else {
-    alert("Isn't even")
-}
+
+var isFalsy = 0
+
+var start = isFalsy || 1
+
+console.log(start)

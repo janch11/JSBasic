64bc0e846afc4624e1ddfb79140afd8f8381aa55
Jan Choma <jandominikchoma@gmail.com>
2018-04-06 16:16:29 +0200

    create nev array with random numbers and random length

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var array = []
 
-array.length = 6 + Math.ceil(Math.random()*10)
 
-for (var i = 0; i < array.length; i++){
-    array[i]=Math.floor(Math.random()*10)}
+var a = Math.ceil(Math.random()*10)
+
+for (var i = 0; i < a; i++){
+
+    var random = Math.floor(Math.random()*10)
+
+    if (random%2 === 0 ){
+    array.push(random)}}
+
     console.log(array)
+
+
+

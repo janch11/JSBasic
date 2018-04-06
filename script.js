041a91bec1b0e3af30fb1f8b64d3fecec3a7e95d
Jan Choma <jandominikchoma@gmail.com>
2018-04-06 11:31:06 +0200

    conditional expression

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-var age = prompt()
-var ageInteger = parseInt(age)
-
-switch (ageInteger) {
-    case 1:
-        console.log(1)
-        break;
-    case "1":
-        console.log(2)
-        break;
-    case 2:
-        console.log(3)
-        break;
-    default:
-        console.log(0)
-}
+expression = true
+console.log(expression? 'This is true' : 'This is false')

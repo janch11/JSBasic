3dd3453045b09662f88e1d79630a92268b009256
Jan Choma <jandominikchoma@gmail.com>
2018-04-06 14:06:12 +0200

    two loops for and while with milkTemp

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ var a = Math.random()
 while(milkTemp < 100) {
 
      milkTemp = milkTemp + a
-    console.log(milkTemp)
+
 
 }
 alert (milkTemp);
+
+for(var milkTempTwo = 20; milkTempTwo < 100; milkTempTwo += a){
+    console.log(milkTempTwo)}

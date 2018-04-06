e8f0ee280bd72835bd018f007296d1c8fe340883
Jan Choma <jandominikchoma@gmail.com>
2018-04-06 14:29:54 +0200

    two loops with creating new 100 arguments arrays changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var a = new Array()
 
-for(i=0; i < 100; i++)
+for(var i = 0; i < 100; i++)
 {
 
 
@@ -19,7 +19,7 @@ for(i=0; i < 100; i++)
 
 var b = []
 
-for(i=0; i < 100; i++)
+for(var i = 0; i < 100; i++)
 {
 
 

e95aee1228f2390e28e402ca89cbae67901d8c5e
Jan Choma <jandominikchoma@gmail.com>
2018-04-10 10:40:24 +0200

    myCar object with {}

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var myCar = new Object ()
 
-myCar.year = 2004
-myCar.make = "Opel"
-myCar.model = "Vectra"
-myCar.engine = 1.9
+
+var myCar =
+{
+    year:  2004,
+    make: "Opel",
+    model: "Vectra",
+    engine: 1.9
+}
 
 console.log(myCar)

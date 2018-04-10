2eec96b4db8f4025e9f144c3e5de7ab0ab0ab982
Jan Choma <jandominikchoma@gmail.com>
2018-04-10 12:14:05 +0200

    IIFE

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 
-var counter = 8
-
-function counterIncrease (){
-    return counter++
-    }
-
-    console.log(counterIncrease())
-    console.log(counterIncrease())
-    console.log(counterIncrease())
-    console.log(counterIncrease())
-    console.log(counterIncrease())
-    console.log(counterIncrease())
 
 
+(function() {
+    var counter = 0
+        function counterIncrease (a,b){
+
+        counter++
+        return a+b
+    }console.log(counterIncrease(5,9))
+    console.log(counter)
+})()
+console.log(counter)
+console.log(counterIncrease)

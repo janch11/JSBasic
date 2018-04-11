b0401f74acd18c157787d3ee98fe782fce7164e4
Jan Choma <jandominikchoma@gmail.com>
2018-04-11 13:44:37 +0200

    classList .add .remove and className

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-function addDiv (content) {
+function addClass () {
 
-    if(content===undefined){
-        content="Hello"}
+    var newDiv = document.querySelector("div")
 
-    var newDiv = document.createElement("div")
-    var newDiv2 = document.createElement("div")
-    var textNode = document.createTextNode(content)
+    newDiv.className = "new2"
 
+    newDiv.classList.add("new")
+    newDiv.classList.add("new1")
+    newDiv.classList.remove("new1")
 
-    newDiv2.appendChild(textNode)
-    newDiv.appendChild(newDiv2)
-    document.body.appendChild(newDiv)
-}
-
-
-
-function addDivByInnerHtml() {
-    document.body .innerHTML = "<div><div>Hello it's me </div></div>"
-
-}
-addDiv()
-
-addDivByInnerHtml()
-
-
-
-
-
+}addClass()

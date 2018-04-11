f576f327ceb87cb476cef6d71fab8e051a537efe
Jan Choma <jandominikchoma@gmail.com>
2018-04-11 11:23:20 +0200

    get element/elements by ID or ClassName and guerrySelector with and wihout all

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ document.getElementsByClassName("text")
 var text = first.getElementsByClassName("text")
 
 
+var querryTextAll = document.querySelectorAll("p.text")
+var querryText = document.querySelector("p.text")
+
+
 console.log(first)
 console.log(text)
+console.log(querryTextAll)
+console.log(querryText)
+

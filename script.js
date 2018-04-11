84598ce936cf2ca02259b131ec2ac76c35e31f83
Jan Choma <jandominikchoma@gmail.com>
2018-04-11 12:26:42 +0200

    create div in div and "Hello" in second div with type parametr

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function addDiv () {
+function addDiv (content) {
+
+    if(content===undefined){
+        content="Hello"}
 
     var newDiv = document.createElement("div")
     var newDiv2 = document.createElement("div")
-    var textNode = document.createTextNode("Hello")
+    var textNode = document.createTextNode(content)
     newDiv.appendChild(newDiv2)
     newDiv2.appendChild(textNode)
 

b96eba47de24ae362915b9def87bd97dd94e7615
Jan Choma <jandominikchoma@gmail.com>
2018-04-11 12:06:58 +0200

    add new element with text to body with change parametr

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function addElement() {
+function addElement(content) {
+
+    content = content === undefined ? "Ala ma kota": content
+
     var bodyElement = document.querySelector("body")
     var newDiv = document.createElement("div")
-    var textNode = document.createTextNode("Ala ma kota")
+    var textNode = document.createTextNode(content)
     newDiv.appendChild(textNode)
     bodyElement.appendChild(newDiv)
 

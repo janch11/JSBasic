3c5937dc5b19a6e9a12341f87c69fbae09c8b42a
Jan Choma <jandominikchoma@gmail.com>
2018-04-11 10:12:22 +0200

    Alerts confrim and prompt IIFE

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function askMe() {
+(function askMe() {
 
     var a = confirm("Czy chcesz podac swoje imie")
 
-    if (a === true) {
+    if (a === true) // You can use only a becouse a is true of false
+    {
         var imie = prompt("Podaj imie!")
         alert("Twoje imie to " + imie)
     }
     else
         alert("No to nie")
-}
+})()
 
-askMe()
+
+
+
+
+
+

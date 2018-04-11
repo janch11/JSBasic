4dddd7b79e239d6bbf74b29358e5b21e81cea2bb
Jan Choma <jandominikchoma@gmail.com>
2018-04-11 10:53:10 +0200

    window location

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-(function askMe() {
+(function add () {
 
-    var a = confirm("Czy chcesz podac swoje imie")
-
-    if (a === true) // You can use only a becouse a is true of false
-    {
-        var imie = prompt("Podaj imie!")
-        alert("Twoje imie to " + imie)
-    }
-    else
-        alert("No to nie")
+     window.location = prompt("Wpisz lokalizacje strony")
 })()
-
-
-
-
-
-
-

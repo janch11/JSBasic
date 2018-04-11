cbd8050c7bef253a2f3f6f04b8f981fb9aded0f7
Jan Choma <jandominikchoma@gmail.com>
2018-04-11 14:16:22 +0200

    set color body to red

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-var newInput
 
-
-
-function addAttribute () {
-
-    newInput = document.querySelector("input")
-
-    newInput.setAttribute("disabled", "")
-    newInput.setAttribute('name', 'email')
-
-    newInput.removeAttribute("disabled")
-
-
-}
-
-addAttribute()
-
-function toggleAttribute () {
-
-    if( newInput.getAttribute("disabled")===null)
-        newInput.setAttribute("disabled", "")
-    else
-        newInput.removeAttribute("disabled")
-
-} toggleAttribute()
+(function changeColor () {
+document.body.style.backgroundColor = "red"}) ()
